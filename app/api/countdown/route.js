4b4db015cac300deb08c7adb6706d858832efe08
ridikhost <153367286+ridikhost@users.noreply.github.com>
2026-02-14 09:08:30 +0100

    Update route.js

--- app/api/countdown/route.js
+++ app/api/countdown/route.js
@@ -3,9 +3,9 @@ export const runtime = 'edge';
 const START_TIME = new Date("2026-02-14T09:10:02Z");
 
 const END_TIME = new Date(
-  START_TIME.getFullYear() + 1,
+  START_TIME.getFullYear() + 2,
   START_TIME.getMonth() + 2,
-  START_TIME.getDate() + 12,
+  START_TIME.getDate() + 5,
   START_TIME.getHours() + 6,
   START_TIME.getMinutes() + 3,
   START_TIME.getSeconds() + 30
